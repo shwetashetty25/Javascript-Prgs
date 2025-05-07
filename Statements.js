4b4c2f052bf7ac650131629e11e24f9a08bc4b84
Shweta Shetty <shettyshweta48@gmail.com>
2025-05-07 09:59:32 +0530

    Add files via upload

---
+++ Statements.js
@@ -0,0 +1,14 @@
+let x = 5;
+
+if (x === 5) {
+    console.log("x is equal to 5");
+} else {
+    console.log("x is not equal to 5");
+}
+
+x = 3; 
+if (x > 2) {
+    console.log("x is greater than 2");
+} else {
+    console.log("x is not greater than 2");
+}
