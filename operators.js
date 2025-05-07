3cd277b7c3ef375e203162febd9efe005c8c53e0
Shweta Shetty <shettyshweta48@gmail.com>
2025-05-07 09:59:13 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,50 @@
+alert("Operator Demo Started!");
+let add = 5 + 3;        
+let sub = 10 - 4;       
+let mul = 2 * 6;        
+let div = 8 / 2;        
+let mod = 10 % 3;       
+let exp = 2 ** 4;       
+
+let x = 10;
+x += 5;    
+x -= 2;    
+x *= 2;    
+x /= 2;    
+x %= 4;    
+x **= 3;   
+
+let a = 5;
+let b = "5";
+let eq1 = (a == b);     
+let eq2 = (a === b);    
+let neq1 = (a != b);    
+let neq2 = (a !== b);   
+let gt = (a > 3);       
+let lt = (a < 10);      
+let gte = (a >= 5);    
+let lte = (a <= 4);   
+
+let andOp = (true && false); 
+let orOp = (true || false); 
+let notOp = !true;        
+
+let y = 5;
+let preInc = ++y;       
+let postDec = y--;      
+let typeOfVal = typeof y;  
+let negate = -y;        
+
+let age = 17;
+let result = (age >= 18) ? "Adult" : "Minor";  
+
+let greet = "Hello, " + "World!";             
+let name = "Alice";
+let welcome = `Welcome, ${name}!`;            
+
+let bitAnd = 5 & 1;    
+let bitOr = 5 | 1;      
+let bitXor = 5 ^ 1;     
+let bitNot = ~5;       
+let leftShift = 5 << 1; 
+let rightShift = 5 >> 1;
