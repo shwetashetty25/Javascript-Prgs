bb5de65490031dc28c6d90cdb4b49735da83a6f5
Shweta Shetty <shettyshweta48@gmail.com>
2025-05-07 09:57:46 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,17 @@
+alert("hello my friend");
+
+let x = 5;
+let y = 10e40;
+let z = "hello";
+let isTrue = true;
+let arr = [1, 2, 3, 4, 5];
+let obj = { name: "John", age: 30 };
+let func = function() { return "hello world"; };
+let nullValue = null;
+let undefineValue;
+let symbolValue = Symbol("w");
+let bigIntValue = BigInt(12344567878);
+let dateValue = new Date();
+let Demo = `the sum of numbers is ${3+5}`;
+
+console.log(Demo);
