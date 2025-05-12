981f50f8b265ad07f5d410d3e7fdbb475c432cec
Shweta Shetty <shettyshweta48@gmail.com>
2025-05-12 11:14:15 +0530

    Add files via upload

---
+++ dayfinder.js
@@ -0,0 +1,26 @@
+let dayNumber = prompt("Enter a number (1 to 7) to get the day:");
+dayNumber = Number(dayNumber); 
+switch(dayNumber) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Invalid input. Enter a number between 1 and 7.");}
