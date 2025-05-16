a02e08fda0a9a53761efa0ad60363ea03b81f891
Shweta Shetty <shettyshweta48@gmail.com>
2025-05-16 12:15:10 +0530

    Add files via upload

---
+++ arrayfunc.js
@@ -0,0 +1,12 @@
+// arr = [1,2,3,4,5,6,7,8,9]
+// console.log(arr[0])
+// console.log(arr[-1])
+// const arr1= new Array("this", "is", "a", "string")
+// console.log(arr[10])
+
+for (let i = 0; i< Array.length; i++){
+    console.log(arr[i])
+}
+for( x of arr) {
+    console.log(x)
+}
