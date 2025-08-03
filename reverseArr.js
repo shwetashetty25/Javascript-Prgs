e14ee9bd0d6fd27b95760d9dcc3cd100df8e7c23
Shweta Shetty <shettyshweta48@gmail.com>
2025-08-03 19:33:27 +0530

    Add files via upload

---
+++ reverseArr.js
@@ -0,0 +1,6 @@
+original = [1, 2, 3, 4, 5];
+reversed = [];
+for (i = original.length - 1; i >= 0; i--) {
+reversed.push(original[i]);}
+console.log("Original", original);  
+console.log("Reversed", reversed);
